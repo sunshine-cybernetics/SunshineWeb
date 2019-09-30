117098a43bec7ab7287331159eb36052bcc0a100
Maisa <maisa.milena@gmail.com>
2019-09-30 14:25:35 -0300

    Adjust clickable Title

--- src/assets/theme/components/Header.js
+++ src/assets/theme/components/Header.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { styles, constants } from '../constants';
 import sunshineLogo from '../../images/Sunshine-logo.png';
 import mainImage from '../../images/main-image.png';
+import headerBg from '../../images/header-bg.png';
 import Menu from './Menu';
 import Title from './Title';
 
@@ -11,18 +12,22 @@ const link = `https://warwick.ac.uk/fac/arts/english/currentstudents/undergradua
 
 const Header = ({ onPressMenuItem, teamRef, aboutRef, contactRef }) => (
   <div style={styles.header}>
-   {/* Left Side */}
-   <div class="HeaderLeft" style={styles.headerLeftSideBox}>
+    {/* Left Side */}
+    <div class="HeaderLeft" style={styles.headerLeftSideBox}>
       <img src={sunshineLogo} alt="sunshine logo" style={styles.headerLogo} />
 
       <div style={{ marginTop: `30%` }}>
-        <Title text="Our best machines are made of Sunshine" />
+        <Title
+          text="Our best machines are made of Sunshine"
+          onClick={() => window.open(link, `_blank`)}
+          clickable
+        />
         <p style={{ color: constants.lightest }}>
           We are part of the team building Moonad: a Peer-to-Peer Formal Operating System
         </p>
       </div>
     </div>
-  {/* Right side */}
+    {/* Right side */}
     <div class="HeaderRight" style={{ ...styles.headerRightSideBox, backgroundImage: `url(${headerBg})` }}>
       <Menu
         onPressMenuItem={onPressMenuItem}
