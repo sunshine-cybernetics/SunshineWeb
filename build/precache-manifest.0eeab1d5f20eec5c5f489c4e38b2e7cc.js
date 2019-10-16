c3104d89a81bd2e7a2e9ecd6453b0c429d2e35f9
Maisa <maisa.milena@gmail.com>
2019-10-16 16:16:22 -0300

    Remove Contact from Header

---
+++ build/precache-manifest.0eeab1d5f20eec5c5f489c4e38b2e7cc.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b476c9d6ce0b47dda695a9cf35480b37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adf2abd8e0ad87054e0d",
+    "url": "/static/css/main.4e1edd0e.chunk.css"
+  },
+  {
+    "revision": "b5eb0519319462062966",
+    "url": "/static/js/2.8636ccb5.chunk.js"
+  },
+  {
+    "revision": "adf2abd8e0ad87054e0d",
+    "url": "/static/js/main.d692ba6e.chunk.js"
+  },
+  {
+    "revision": "4e8d1f41da644c87b4e1",
+    "url": "/static/js/runtime~main.376fb1ac.js"
+  },
+  {
+    "revision": "cf50dfdc3875c9c13a169d93b65d6479",
+    "url": "/static/media/Bernardo-Amorim.cf50dfdc.jpg"
+  },
+  {
+    "revision": "d4900d4a99b04a043e7b83ae19154148",
+    "url": "/static/media/Formality.d4900d4a.png"
+  },
+  {
+    "revision": "7ced46881e64443effb62868119209d8",
+    "url": "/static/media/Gabriel-Barreto.7ced4688.jpg"
+  },
+  {
+    "revision": "1c2af99ef056a42bf16c6a1675beaf0f",
+    "url": "/static/media/John-Burnham.1c2af99e.jpg"
+  },
+  {
+    "revision": "899da61eef883e56f1d7d66b843ed58f",
+    "url": "/static/media/Leonardo-Souza.899da61e.jpg"
+  },
+  {
+    "revision": "964029dc9e7157afd10a33f032a3d335",
+    "url": "/static/media/Maisa-Milena.964029dc.jpg"
+  },
+  {
+    "revision": "e69e22cf6b55c570b33c1d4d01acd4e9",
+    "url": "/static/media/Moonad.e69e22cf.png"
+  },
+  {
+    "revision": "caede3e96afba8c8f49e95dac09ac957",
+    "url": "/static/media/Phoebe-Burnham.caede3e9.jpg"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "14bd3f1ebe3df1192354f15c4d4e3f4a",
+    "url": "/static/media/Sunshine-logo.14bd3f1e.png"
+  },
+  {
+    "revision": "95782905957f6cd58edc22130b652b08",
+    "url": "/static/media/Victor-Maia.95782905.jpg"
+  },
+  {
+    "revision": "b2e716926452ad3b7047d2f0dec1fca7",
+    "url": "/static/media/aaron-stump.b2e71692.svg"
+  },
+  {
+    "revision": "b3555e90c757fe64f7f80176be4593c1",
+    "url": "/static/media/bg-team-sunshine.b3555e90.svg"
+  },
+  {
+    "revision": "bebff4bed28713726abebd56fc3ec944",
+    "url": "/static/media/contact-us-bg.bebff4be.png"
+  },
+  {
+    "revision": "e83ad4a4c4a18cf41599ceaec4f8339f",
+    "url": "/static/media/ethereum-foundation.e83ad4a4.svg"
+  },
+  {
+    "revision": "ff1d322daa20d811d983e49ee7a0a4a3",
+    "url": "/static/media/github.ff1d322d.png"
+  },
+  {
+    "revision": "061477e98d8c7d491bff91e1ad040962",
+    "url": "/static/media/header-bg.061477e9.png"
+  },
+  {
+    "revision": "a1fb19626a53813ac38678bb5a0d731e",
+    "url": "/static/media/jacob-arluck.a1fb1962.svg"
+  },
+  {
+    "revision": "4417cb9a20bff78d53db861b9253b645",
+    "url": "/static/media/main-image.4417cb9a.png"
+  },
+  {
+    "revision": "ebde6db9f4a2fc50afd71b417459eced",
+    "url": "/static/media/telegram.ebde6db9.png"
+  }
+]);
